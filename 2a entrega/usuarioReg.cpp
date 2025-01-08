d60797f8e3916a7aede5824625f8aba129f8311d
albertoosg <152171994+albertoosg@users.noreply.github.com>
2025-01-08 19:49:49 +0100

    Add files via upload

---
+++ 2a entrega/usuarioReg.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <exception>
+#include "usuarioReg.hpp"
+
+using namespace std;
+
+string usuarioRegistrado :: get_nombreUsuario(){ //Getter de la clase usuarioRegistrado para obtener la variable nombreUsuario
+        return nombreUsuario;
+}
+
+string usuarioRegistrado :: get_contrasena(){ //Getter de la clase usuarioRegistrado para obtener la variable contrasena
+    return contrasena;
+}
+
+void usuarioRegistrado :: set_nombreUsuario(string nombreUsuario_nuevo){ //Setter de la clase usuarioRegistrado para actualizar la variable nombreUsuario
+    nombreUsuario = nombreUsuario_nuevo;
+}
+
+void usuarioRegistrado :: set_contrasena(string contrasena_nueva){ //Setter de la clase usuarioRegistrado para actualizar la variable contrasena
+        contrasena = contrasena_nueva;
+    }
